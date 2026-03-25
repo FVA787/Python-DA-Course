d9586f8126333c0e567e45f4cbc73c8669cdaf4c
VLADIMIR FOMICHEV <166392515+FVA787@users.noreply.github.com>
2026-03-25 18:01:23 +0300

    исправили нерабочий код, добавили двоеточие после else:

--- Homeworks/DZ_1.1.py
+++ Homeworks/DZ_1.1.py
@@ -7,5 +7,5 @@ if len(phrase_1) > len(phrase_2):
     print("Фраза 1 длиннее фразы 2")
 elif len(phrase_1) < len(phrase_2):
     print("Фраза 2 длиннее фразы 1")
-else
+else:
     print ("Фразы одинаковой длины")
